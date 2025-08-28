bb5d51601f9fbe48356afd83f09a63751ed299cd
simekke <simekke@pwcs.edu>
2025-08-28 11:14:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,12 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
+
         # Display smootiefroot nutrition information
+        # Utilize Search term rather than menu terms
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+              
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
